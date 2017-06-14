1b4c7e1bdcb97eb34e93f806c6349c833231c4d0
Yen <yenchieh86@hotmail.com>
2017-06-14 20:55:56 +0000

    change email info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,22 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blocmetrics_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.default_url_options = { host: "https://yen-metrics.herokuapp.com/" }
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.perform_deliveries = true
+  
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV["GMAIL_USERNAME"],
+    password:       ENV["GMAIL_PASSWORD"],
+    address:        'smtp.gmail.com',
+    domain:         'heroku.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
